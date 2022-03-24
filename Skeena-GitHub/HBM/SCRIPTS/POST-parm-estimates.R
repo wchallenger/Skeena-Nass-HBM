6d3ef4214e773570fd76c9c089fe801f4b2e7e74
Wendell Challenger <wchallenger@lgl.com>
2022-03-24 14:20:16 -0700

    Creates a results object with run estimates
    For use by Gottfried in his comparison plots.

---
+++ Skeena-GitHub/HBM/SCRIPTS/POST-parm-estimates.R
@@ -0,0 +1,49 @@
+# Generate 
+
+source("~/GitHub/Fisheries/Skeena-Nass-HBM/Skeena-GitHub/HBM/FUNCTIONS/ComputeTimeVaryingAlpha.fn.R")
+
+ver <- "2022-03-24"
+
+result.files <- list(
+  'Base Case' = "result_HBM_Skeena_m23.rds",
+  'nonHBM' = "result_HBM_Skeena_m23_nonHBM.rds",
+  'Senitivity Run 1' = "result_HBM_Skeena_m24.rds",
+  'Senitivity Run 2' = "result_HBM_Skeena_m25.rds",
+  'Senitivity Run 3' = "result_HBM_Skeena_m26.rds",
+  'Senitivity Run 4' = "result_HBM_Skeena_m27.rds",
+  'Senitivity Run 5' = "result_HBM_Skeena_m28.rds",
+  'Senitivity Run 6' = "result_HBM_Skeena_m29.rds"
+)
+
+
+HBM.results <- list()
+
+for (r in seq_along(result.files)) {
+  run <- names(result.files)[r]
+  message("Processing Run:", run)
+  
+  mod.result <- readRDS(file.path("HBM", "RESULTS", ver, result.files[r]))
+  
+  HBM.results[[run]] <- list()
+  
+  HBM.results[[run]][['estimates']] = mod.result$estimates %>%
+    add_column(median = NA, .after = "mean") %>%
+    mutate(median = `50%`) %>%
+    select(-`50%`) %>%
+    mutate(
+      Parm = str_replace(Parm, "^intercept$", "ln.alpha"),
+      Parm = str_replace(Parm, "^intercept_c$", "ln.alpha.c"),
+      Parm = str_replace(Parm, "^slope$", "beta")
+    ) %>%
+    rename(VarType = Parm) %>%
+    filter(VarType %in% c("deviance", "ln.alpha", "ln.alpha.c", "beta", "TE", "Smax", "Smsy", "Umsy" ))
+   
+    # HBM.results[[run]][['estimates']] %>% count(VarType)
+  
+  message("  Deriving time varying alpha")
+  HBM.results[[run]][['time.var.alpha']] <- ComputeTimeVaryingAlpha(mod.result)
+  
+}
+
+out.file <- file.path(save.dir, "HBM_results.rds")
+saveRDS(HBM.results, out.file)
