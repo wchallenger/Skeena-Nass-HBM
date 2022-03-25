e4cf9bd322daa13ee76c49bef6ba8486e4b8f9a8
Wendell Challenger <wchallenger@lgl.com>
2022-03-25 16:42:27 -0700

    Updated data, used some post analyses
    - Now uses SandBox repo data.
    - Added calls to scripts for  comparing low/high productivity periods
    - Moved logic to save appendix tables as excel to a dedicated script.

--- Skeena-GitHub/HBM/SCRIPTS/_RUN_ALL_ANALYSES.R
+++ Skeena-GitHub/HBM/SCRIPTS/_RUN_ALL_ANALYSES.R
@@ -10,14 +10,17 @@ library(MCMCvis)
 
 # base.dir <- "~/GitHub/Fisheries/Skeena-Nass-Sockeye-Model-Sandbox"
 setwd("~/GitHub/Fisheries/Skeena-Nass-HBM/Skeena-GitHub")
+base.dir <- "."  # assumed working directory is set to project root.
 
+base.dir <- "~/GitHub/Fisheries/Skeena-Nass-HBM/Skeena-GitHub"
 
 # Settings ----------------------------------------------------------------
 ver <- as.character(Sys.Date())
+# ver <- "2022-03-24"
 
-base.dir <- "."  # assumed working directory is set to project root.
 
-data.dir <- file.path(base.dir, "DATA")
+# data.dir <- file.path(base.dir, "DATA")
+data.dir <- "/Users/wchallenger/GitHub/Fisheries/Skeena-Nass-Sockeye-Model-Sandbox/1_DATA"
 script.dir <- file.path(base.dir, "HBM", "SCRIPTS")
 jags.dir <- file.path(base.dir, "HBM", "JAGS")
 fun.dir <- file.path(base.dir, "HBM", "FUNCTIONS")
@@ -37,6 +40,12 @@ sr.scale <- 1
 if (!dir.exists(save.dir)) {
   
   dir.create(save.dir)
+  
+  # Prepare main analysis data set used across all analyses
+  source(file.path(script.dir, "PREP-analysis_data.R"))
+  
+  # Determine convergence of basecase for fundamental parameters
+  source(file.path(script.dir, "RUN-basecase_diagnostics.R"))
   
   # Run basecase, the main sensitivity analyese, and nonHBM model for shrinkage
   source(file.path(script.dir, "RUN-basecase_nonHBM_runs1-6.R"))
@@ -59,82 +68,36 @@ app.tables[['Smax']] <- smax.tab %>% select(Basin, Stock, Lake, prSmax, prCV) %>
 # Create shrinkage table and plot
 source(file.path(script.dir, "POST-shrinkage.R"))
 
+# Create sensitivity of reference points to high and low productivity
+source(file.path(script.dir, "POST-productivity_periods.R"))
+
 # Create comparison tables for sensitivity runs 1-6
 source(file.path(script.dir, "POST-sensitivity_comparisons.R"))
 
-# Save to file
+# Save a copy of appendix tables to Excel
+source(file.path(script.dir, "SAVE-appendix_tables_excel.R"))
+
+# Save RDS for report integration
 for (table in names(app.tables)) {
   colnames(app.tables[[table]]) <- str_replace_all(colnames(app.tables[[table]]), "[:space:]", "\\.")
 }
 saveRDS(app.tables, file.path(output.dir, "HBM_appendix_tables.rds"))  # Tables
 
 
-# Create jackknife figures using sensitivity runs 7-25
+# Generate jackknife figures using sensitivity runs 7-25
 source(file.path(script.dir, "POST-jackknife_plots.R"))
 
-# Create year effect plots
+# Generate year effect plots
 source(file.path(script.dir, "POST-shared_yr_effect.R"))
 
-
 # saveRDS(app.plots, file.path(output.dir, "HBM_appendix_figures.rds")) # Figures
-# Post Processing: Estimates ----------------------------------------------
 
+# Post Processing: Estimates ----------------------------------------------
+# Create a nested list of parameter estimates and time varying alpha
+# estimates by 
 
 message("Generating parameter estimate summaries for main report")
 source(file.path(script.dir, "POST-parm-estimates.R"))
 
 
-# Save Excel Copy -------------------------------------------------------------
-wb <- createWorkbook()
 
-for (run in names(app.tables)) {
-  
-  tab <- app.tables[[run]]
-  
-  addWorksheet(wb, run)
-  writeData(wb, run, tab)
-  
-  # header
-  style <- createStyle(
-    border= c("Bottom", "Top"), 
-    fgFill = rgb(208,208,208, maxColorValue = 255),
-    textDecoration = "bold",
-    wrapText = TRUE
-  )  
-  addStyle(wb, run, style, rows= 1, cols = 1:ncol(tab), gridExpand = TRUE)
-  
-  if (str_detect(run, "Shrinkage")) {
-    style <- createStyle(numFmt = "0.000")
-    addStyle(wb, run, style, rows= 2:(nrow(tab) + 1), cols = c(2:5), gridExpand = TRUE)
-    style <- createStyle(numFmt = "0.0%")
-    addStyle(wb, run, style, rows= 2:(nrow(tab) + 1), cols = c(6:7), gridExpand = TRUE)
-  }
-  
-  if (str_detect(run, "Run")) {
-  
-    # Percent change column
-    check1 <- run %in% c( "Run 2 Smsy", "Run 3 Smsy")
-    check2 <-  str_detect(run, "Umsy")
-    if (check1 | check2) {
-      style <- createStyle(numFmt = "0.0%")
-    } else {
-      style <- createStyle(numFmt = "0%")
-    }
-    addStyle(wb, run, style, rows= 2:(nrow(tab) + 1), cols = c(4,7), gridExpand = TRUE)
-    # Format estimates
-    if (run == "Run 1" | str_detect(run, "Umsy")) {
-      style <- createStyle(numFmt = "0.000")
-    } else {
-      style <- createStyle(numFmt = "#,##0")
-    }
-    addStyle(wb, run, style, rows= 2:(nrow(tab) + 1), cols = c(2:3,5:6), gridExpand = TRUE)
-    
-    
-    setColWidths(wb, run, cols = 1:7, widths = 16.5) 
-  }
-  
-  
-}
-
-out.file <- file.path(output.dir, paste0("appendix_results_JAGS--", ver, ".xlsx"))
-saveWorkbook(wb, file = out.file, overwrite = TRUE)
